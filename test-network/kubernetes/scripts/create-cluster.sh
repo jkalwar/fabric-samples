eeea576999a3fc57e533e66ec572ee5e805f696a
Jayakrishna Alwar <alwar.jayakrishna11011@gmail.com>
2021-04-13 17:17:35 +0530

    Updates

--- test-network/kubernetes/scripts/create-cluster.sh
+++ test-network/kubernetes/scripts/create-cluster.sh
@@ -71,11 +71,11 @@ az aks create \
 
 
 #kubectl is the main Kubernetes command-line client you use to interact with your cluster and is available in Cloud Shell. A cluster context is required to allow kubectl to connect to a cluster. The context contains the cluster's address, a user, and a namespace. Use the az aks get-credentials command to configure your instance of kubectl.
-
 az aks get-credentials \
     --resource-group $RESOURCE_GROUP \
     --name $AKS_CLUSTER_NAME
 
+#To test if the kubectl commands are working.
 kubectl get nodes
 
 
